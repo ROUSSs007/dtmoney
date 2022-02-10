c6f325f65bd7714cd7164e451631fbf22269b40a
Gabriel Rousselet <gabrielprousselet@gmail.com>
2022-02-10 14:00:45 -0300

    Changing the modal styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { Dashboard } from "./components/Dashboard";
+import Modal from 'react-modal'
 import { Header } from "./components/Header";
+import { useState } from "react";
+
 import { GlobalStyle } from "./styles/global";
+import NewTransactionModal from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactioModalOpen, setIsNewTransactioModalOpen] = useState (false);
+
+  function handleOpenNewTransactioModal() {
+      setIsNewTransactioModalOpen(true);
+  }
+
+  function handleCloseNewTransactioModal() {
+      setIsNewTransactioModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactioModal}/>
+
       <Dashboard />
+      
+      <NewTransactionModal 
+        isOpen={isNewTransactioModalOpen} 
+        onRequestClose={handleCloseNewTransactioModal} 
+      />
+
       <GlobalStyle />
     </>
   );
